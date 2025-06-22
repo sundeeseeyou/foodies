c10a7f618a64f9076c69b81fc05ccf8eccc49f65
Sandi Miftah <sandiseeyou@gmail.com>
2025-06-22 22:46:51 +0700

    Adding not found handler

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { TbError404 } from "react-icons/tb";
+
+export default function NotFound() {
+  return (
+    <main className="flex flex-col gap-4 justify-center items-center min-h-[80svh]">
+      <TbError404 className="text-red-600 text-6xl" />
+      <h1 className="text-6xl text-black font-bold">NOT FOUND 404</h1>
+      <p>Oops. Please try another query.</p>
+    </main>
+  );
+}
