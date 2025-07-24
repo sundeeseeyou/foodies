bafb6332010b47d4ba6aa72c68971898bd02024a
Sandi Miftah <sandiseeyou@gmail.com>
2025-07-24 23:57:56 +0700

    Super late commit, fix layout remove unnecessary comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,7 @@
 import type { Metadata } from "next";
-// import { Nunito_Sans, Fira_Mono } from "next/font/google";
 import "./globals.css";
 import HeaderFoodies from "@/components/homepage/HeaderFoodies";
 import Footer from "../components/homepage/Footer";
-
-// const nunitoSans = Nunito_Sans({
-//   variable: "--font-nunito-sans",
-//   subsets: ["latin"],
-//   display: "swap",
-// });
-
-// const firaMono = Fira_Mono({
-//   variable: "--font-fira-mono",
-//   subsets: ["latin"],
-//   weight: "400",
-//   display: "swap",
-// });
 
 export const metadata: Metadata = {
   title: "Food Network",
@@ -29,9 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body
-        className={`${nunitoSans.variable} ${firaMono.variable} flex flex-col h-full min-h-screen mx-auto items-center font-[family-name:var(--font-nunito-sans)] antialiased`}
-      > */}
       <body className="flex flex-col h-full min-h-screen mx-auto items-center antialiased font-display ">
         <HeaderFoodies />
         {children}
