9f700c75332f80a635764e6d56184725c3c0558e
Sandi Miftah <sandiseeyou@gmail.com>
2025-08-26 17:33:20 +0700

    feat(server): fix metadata 3

--- src/app/meals/[slug]/not-found.tsx
+++ src/app/meals/[slug]/not-found.tsx
@@ -1,12 +1,19 @@
 import { TbError404 } from "react-icons/tb";
 import Link from "next/link";
+import { Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "Recipe Not Found",
+  description: "The recipe you’re looking for doesn’t exist or was removed.",
+  robots: { index: false, follow: false },
+};
 
 export default function NotFound() {
   return (
     <main className="flex flex-col gap-4 justify-center items-center min-h-[80svh]">
       <TbError404 className="text-red-600 text-9xl" />
       <h1 className="text-6xl text-black font-bold">RECIPE IS NOT FOUND</h1>
-      <p>Oops. Please try another query.</p>
+      <p>Please try another recipe.</p>
       <Link href={"/meals"} className="text-gray-600 text-sm hover:underline">
         Back
       </Link>
--- src/app/meals/[slug]/page.tsx
+++ src/app/meals/[slug]/page.tsx
@@ -2,16 +2,19 @@ import { notFound } from "next/navigation";
 import { getRecipe } from "@/lib/_meals";
 import RecipeDetail from "@/components/meals/_RecipeDetails";
 
-type Params = {
-  slug: string;
-};
+type Params = Promise<{ slug: string }>;
 
 export async function generateMetadata({ params }: { params: Params }) {
   const { slug } = await params;
   const recipe = await getRecipe(slug);
 
   if (!recipe) {
-    notFound();
+    return {
+      title: "Recipe Not Found",
+      description:
+        "The recipe you’re looking for doesn’t exist or was removed.",
+      robots: { index: false, follow: false },
+    };
   }
 
   return {
