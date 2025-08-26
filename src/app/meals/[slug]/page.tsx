60e0d2048d73f954eaf286399b7608def769c9de
Sandi Miftah <sandiseeyou@gmail.com>
2025-08-26 17:10:56 +0700

    feat(server): fix metadata

--- src/app/meals/[slug]/page.tsx
+++ src/app/meals/[slug]/page.tsx
@@ -2,6 +2,24 @@ import { notFound } from "next/navigation";
 import { getRecipe } from "@/lib/_meals";
 import RecipeDetail from "@/components/meals/_RecipeDetails";
 
+type Params = {
+  slug: string;
+};
+
+export async function generateMetadata({ params }: { params: Params }) {
+  const { slug } = params;
+  const recipe = await getRecipe(slug);
+
+  if (!recipe) {
+    notFound();
+  }
+
+  return {
+    title: { default: recipe.title, template: "%s | Foodies" },
+    description: recipe.summary,
+  };
+}
+
 export default async function SinglePageRecipe({ params }: { params: Params }) {
   const { slug } = await params;
   const recipe = await getRecipe(slug);
